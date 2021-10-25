71a1c04ab2938a27bca9eaa72e4de4cb3cd8bd03
ksycip <56284512+ksycip@users.noreply.github.com>
2021-10-25 10:53:10 -0700

    final commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+let array = [];
+let strokeWidth = 5;
+let noiseOffset = 0.0;
+let colorPicker;
+let song;
+
+// colorPicker
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+    colorPicker = createColorPicker('#ed225d');
+    colorPicker.position(1370, 10);
+  song = loadSound('assets/pop.wav');
+
 }
 
+// drawing tool
 function draw() {
-  background(220);
+  strokeWeight(10);
+  stroke(colorPicker.color());
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+
+
+// space bar for clear
+function keyPressed() {
+  if (key == ' '){
+  clear();
+  }
+}
+
+function mousePressed(){
+  if (song.isPlaying()) {
+    song.stop();
+  } else { 
+    song.play();
+  }
 }
